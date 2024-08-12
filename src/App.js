97f1abf969297f4f505732f7f4a3697b1d2d012d
Fractalo <42487844+fractalo@users.noreply.github.com>
2024-08-13 07:54:32 +0900

    feat: image gallery

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ScrollToTop from './ScrollToTop';
+import 'simplebar-react/dist/simplebar.min.css';
 
 function App() {
   return (
